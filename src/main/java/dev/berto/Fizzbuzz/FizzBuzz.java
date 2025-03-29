284fff8bf65deacb1b7b2af939a4b089d9f1ff8b
berto9675 <albrtvd@gmail.com>
2025-03-29 17:43:03 +0100

    created the FizzBuzz class with its methods

--- src/main/java/dev/berto/Fizzbuzz/FizzBuzz.java
+++ src/main/java/dev/berto/Fizzbuzz/FizzBuzz.java
@@ -1,5 +1,20 @@
 package dev.berto.Fizzbuzz;
 
 public class FizzBuzz {
+    public String chechkNumber(int number){
+        boolean isDivisibleBy3And5 = number % 3 == 0 && number % 5 == 0;
+        if (isDivisibleBy3And5) {
+            return "FizzBuzz";
+        }
+        boolean isDivisibleBy3 = number % 3 == 0;
+        if (isDivisibleBy3) {
+            return "Fizz";
+        }
+        boolean isDivisibleBy5 = number % 5 == 0;
+        if (isDivisibleBy5) {
+            return "Buzz";
+        }
+        return "null";
+    }
     
 }
--- src/test/java/dev/berto/AppTest.java
+++ src/test/java/dev/berto/AppTest.java
@@ -1,7 +1,6 @@
 package dev.berto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
